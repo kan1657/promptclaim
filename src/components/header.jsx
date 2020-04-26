6fefd7d1621c189dc9afa628bc1cda1959336b0f
AlphaZ <kan1657@gmail.com>
2020-04-26 23:58:58 +0700

    add library pure css

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -29,14 +29,14 @@ export const Topbar = () => {
 }
 const menu = (
   <Menu>
-    <Menu.Item key="0">
+    {/* <Menu.Item key="0">
       <a href="http://www.alipay.com/">1st menu item</a>
     </Menu.Item>
     <Menu.Item key="1">
       <a href="http://www.taobao.com/">2nd menu item</a>
     </Menu.Item>
-    <Menu.Divider />
-    <Menu.Item key="2" onClick={() => localStorage.clear()}>
+    <Menu.Divider /> */}
+    <Menu.Item key="0" onClick={() => localStorage.clear()}>
       <a href="/login">LogOut</a>
     </Menu.Item>
   </Menu>
