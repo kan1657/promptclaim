4b3e77cacc8f4a501cf45a2ea90aa6dc81a2f69d
Sarut Sakulwira <sarutsakulwira@Saruts-MacBook-Pro.local>
2020-04-26 23:53:44 +0700

    fix data bug & change claim info in view product, add start/end date in view policy

--- src/pages/view-policy-page.jsx
+++ src/pages/view-policy-page.jsx
@@ -12,6 +12,8 @@ export const ViewPolicyPage = (props) => {
   const [item, setItems] = useState([])
   const [policyId, setPolicyId] = useState('')
   const [policyDes, setPolicyDes] = useState('')
+  const [start, setStart] = useState('')
+  const [end, setEnd] = useState('')
   const fetchItem = async () => {
     // const data = await fetch(
     //   'http://localhost:8001/retailer/policy/getByPolicyId/' + key,
@@ -32,6 +34,8 @@ export const ViewPolicyPage = (props) => {
     console.log(item)
     await setPolicyId(item[0].policy_id)
     await setPolicyDes(item[0].policy_description)
+    await setStart(item[0].policy_start_date)
+    await setEnd(item[0].policy_end_date)
   }
 
   useEffect(() => {
@@ -51,7 +55,9 @@ export const ViewPolicyPage = (props) => {
           <Descriptions.Item label="Policy Informaion">
             Policy ID: {policyId}
             <br />
+            Policy Start Date: {start.substr(0,10)}
             <br />
+            Policy End Date: {end.substr(0,10)}
             <Collapse>
               <Panel header="Policy Description">{policyDes}</Panel>
             </Collapse>
