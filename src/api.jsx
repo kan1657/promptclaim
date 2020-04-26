aaf234c78d16bd47547b8645a5ccb31877cdc022
AlphaZ <kan1657@gmail.com>
2020-04-26 22:26:10 +0700

    Change API

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL:
-    'http://ec2-54-169-201-208.ap-southeast-1.compute.amazonaws.com:8001',
+  baseURL: 'http://ec2-3-0-20-60.ap-southeast-1.compute.amazonaws.com:8001',
 })
